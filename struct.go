a85c20990cb444ae89a47f154ba40f281a4c3c0d
collin <collin@nb.cx>
2024-08-22 19:19:16 +0800

    improve type analysis

--- struct.go
+++ struct.go
@@ -1,30 +1,35 @@
 package flag
 
 import (
+	"fmt"
+	"net"
 	"reflect"
+	"time"
 )
 
 // StringP is like String, but accepts a shorthand letter that can be used after a single dash.
 func (f *FlagSet) Scan(target any) {
 	sVal, sType := reflect.ValueOf(target), reflect.TypeOf(target)
-
-	if sType.Kind() == reflect.Ptr {
-		//用Elem()获得实际的value
-		sVal = sVal.Elem()
-		sType = sType.Elem()
+	if sType.Kind() != reflect.Ptr {
+		panic("target must be pointer")
 	}
+
+	// only struct type has tag, can parse
+	sVal, sType = sVal.Elem(), sType.Elem()
+	if sType.Kind() != reflect.Struct {
+		panic("target must be struct")
+	}
+
 	for i := 0; i < sType.NumField(); i++ {
 		field := sType.Field(i)
 		tag := field.Tag
 
-		name := tag.Get("name")
-		short := tag.Get("short")
+		name, short := tag.Get("name"), tag.Get("short")
 		if (short == "" && name == "") || !sVal.Field(i).CanSet() {
 			continue
 		}
 		v := sVal.FieldByName(field.Name).Addr().UnsafePointer()
 		var ve Value
-
 		kind := tag.Get("type")
 		if kind == "" {
 			kind = field.Type.String()
@@ -33,20 +38,32 @@ func (f *FlagSet) Scan(target any) {
 		switch kind {
 		case "string":
 			ve = newStringValue("", (*string)(v))
+		case "[]string":
+			ve = newStringArrayValue(nil, (*[]string)(v))
 		case "int":
 			ve = newIntValue(0, (*int)(v))
+		case "[]int":
+			ve = newIntSliceValue([]int{}, (*[]int)(v))
 		case "int8":
 			ve = newInt8Value(0, (*int8)(v))
 		case "int16":
 			ve = newInt16Value(0, (*int16)(v))
 		case "int32":
 			ve = newInt32Value(0, (*int32)(v))
+		case "[]int32":
+			ve = newInt32SliceValue([]int32{}, (*[]int32)(v))
 		case "int64":
 			ve = newInt64Value(0, (*int64)(v))
+		case "[]int64":
+			ve = newInt64SliceValue([]int64{}, (*[]int64)(v))
 		case "uint":
 			ve = newUintValue(0, (*uint)(v))
+		case "[]uint":
+			ve = newUintSliceValue([]uint{}, (*[]uint)(v))
 		case "uint8":
 			ve = newUint8Value(0, (*uint8)(v))
+		case "[]uint8":
+			ve = newBytesHexValue([]byte{}, (*[]byte)(v))
 		case "uint16":
 			ve = newUint16Value(0, (*uint16)(v))
 		case "uint32":
@@ -55,17 +72,33 @@ func (f *FlagSet) Scan(target any) {
 			ve = newUint64Value(0, (*uint64)(v))
 		case "float32":
 			ve = newFloat32Value(0, (*float32)(v))
+		case "[]float32":
+			ve = newFloat32SliceValue([]float32{}, (*[]float32)(v))
 		case "float64":
 			ve = newFloat64Value(0, (*float64)(v))
+		case "[]float64":
+			ve = newFloat64SliceValue([]float64{}, (*[]float64)(v))
 		case "bool":
 			ve = newBoolValue(false, (*bool)(v))
 			if noOptDefVal == "" {
 				noOptDefVal = "true"
 			}
-		case "[]string":
-			ve = newStringArrayValue(nil, (*[]string)(v))
-		case "[]slice":
-			ve = newStringArrayValue(nil, (*[]string)(v))
+		case "[]bool":
+			ve = newBoolSliceValue([]bool{}, (*[]bool)(v))
+		case "net.IP":
+			ve = newIPValue(net.IP{}, (*net.IP)(v))
+		case "[]net.IP":
+			ve = newIPSliceValue([]net.IP{}, (*[]net.IP)(v))
+		case "net.IPMask":
+			ve = newIPMaskValue(net.IPMask{}, (*net.IPMask)(v))
+		case "count":
+			ve = newCountValue(0, (*int)(v))
+		case "time.Duration":
+			ve = newDurationValue(0, (*time.Duration)(v))
+		case "[]time.Duration":
+			ve = newDurationSliceValue([]time.Duration{}, (*[]time.Duration)(v))
+		default:
+			panic(fmt.Sprintf("unsupported parsing type: %v", kind))
 		}
 		if value := tag.Get("value"); value != "" {
 			_ = ve.Set(value)
--- struct_test.go
+++ struct_test.go
@@ -1,28 +1,58 @@
 package flag
 
 import (
-	"fmt"
+	"net"
+	"reflect"
 	"testing"
 )
 
 // name, shorthand string, value string, usage string
-type User struct {
-	Name  string `json:"name"`
-	Age   int    `short:"a" value:"1233"`
-	Hello string `name:"hello" short:"h" value:"hee" usage:"usage"`
-	Sex   bool   `name:"sex" short:"s" def:"false"`
-	Sex2  bool   `name:"sex2" short:"x"`
+type Target struct {
+	Name  string   `name:"name"`
+	Names []string `name:"names"`
+	Yes   bool     `name:"yes" short:"y"`
+	Ok    byte     `name:"ok" short:"o" value:"1" def:"2"`
+	Sexes []bool   `name:"sexes" short:"s"`
+	Count int      `name:"count" short:"c" value:"1" def:"2" type:"count"`
+	Ip    net.IP   `name:"ip" short:"i" value:"127.0.0.1" def:"0.0.0.0"`
 }
 
-func TestBind(t *testing.T) {
-	u := &User{}
-	Scan(u)
-	var b bool
-	BoolVarP(&b, "bb", "b", false, "ddd")
-	// _ = CommandLine.Parse([]string{"--age=13 -s"})
-	_ = CommandLine.Parse([]string{"--sex=true", "-x", "-b"})
-
-	sex, _ := CommandLine.GetBool("sex")
-	fmt.Println(">>>", u.Hello, u.Age, u.Sex, ">>", b, "sex get", sex)
-
+func TestScan(t *testing.T) {
+	target := &Target{}
+	tests := []struct {
+		name  string
+		input []string
+		v1    any
+		v2    any
+	}{
+		{"string", []string{"--name=hello"}, &target.Name, "hello"},
+		{"bool", []string{"--yes=true"}, &target.Yes, true},
+		{"byte", []string{"-o"}, &target.Ok, uint8(2)},
+		{"count", []string{"-c"}, &target.Count, int(2)},
+		{"count", []string{"-c=3"}, &target.Count, int(3)},
+		// {"count", []string{"--ip=192.168.0.1"}, &target.Count, net.IP("192.168.0.1")},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fs := NewFlagSet("Example", ContinueOnError)
+			fs.Scan(target)
+			err := fs.Parse(tt.input)
+			if err != nil {
+				t.Errorf("parse err: %v", err)
+				return
+			}
+			v1 := reflect.ValueOf(tt.v1)
+			v2 := reflect.ValueOf(tt.v2)
+			if v1.Kind() == reflect.Pointer {
+				v1 = v1.Elem()
+			}
+			if v2.Kind() == reflect.Pointer {
+				v2 = v2.Elem()
+			}
+			if !v1.Equal(v2) {
+				t.Errorf("v1 != v2: %v", tt.v2)
+				return
+			}
+		})
+	}
 }
